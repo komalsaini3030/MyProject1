7c4b6f90008289c747cbba2f14d42346da5f6501
Komal Saini <Komal Saini@DESKTOP-MIFMATU>
2020-08-06 09:52:41 +0530

    new class added

---
+++ EmployeeManagementSystem/src/com/lti/VideoGame.java
@@ -0,0 +1,12 @@
+package com.lti;
+
+public class VideoGame {
+  public static void ludo(){
+	  System.out.println("I love playing ludo");
+  }
+	public static void main(String[] args) {
+		ludo();
+
+	}
+
+}
