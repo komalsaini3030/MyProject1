ae6cfbd21ba28608ce05fe8552700995d4bb70ba
Komal Saini <Komal Saini@DESKTOP-MIFMATU>
2020-08-06 09:48:45 +0530

    diff method added

--- EmployeeManagementSystem/src/com/lti/Calculator.java
+++ EmployeeManagementSystem/src/com/lti/Calculator.java
@@ -4,8 +4,12 @@ public class Calculator {
 	public static void add(int a,int b){
 		  System.out.println("Sum= "+(a+b));
 		  }
+	public static void diff(int a,int b){
+		  System.out.println("difference= "+(a-b));
+		  }
 	public static void main(String[] args) {
 		add(100,20);
+		diff(50,4);
 
 	}
 
