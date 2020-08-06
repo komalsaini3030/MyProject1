4400894f20d6af4b007d0b5d800875c0ee5a7e21
Komal Saini <Komal Saini@DESKTOP-MIFMATU>
2020-08-06 10:20:29 +0530

    pubg method added

--- EmployeeManagementSystem/src/com/lti/VideoGame.java
+++ EmployeeManagementSystem/src/com/lti/VideoGame.java
@@ -4,12 +4,10 @@ public class VideoGame {
   public static void ludo(){
 	  System.out.println("I love playing ludo");
   }
-  public static void pubg(){
-	  System.out.println("I love playing pubg");
-  }
+  
 	public static void main(String[] args) {
 		ludo();
-		pubg();
+		//pubg();
 
 	}
 
